aa9e95428abbdae86382cdc60aa4b1f1760638a6
kesavakrishnanm <contact@krishnainfotron.com>
2021-09-08 15:19:41 +0530

    Home Task 1 - Component Creation 1. PureComponet used in counter component 2. Simple class component created for Hello World and elements created using React.creatElement 3. Functional component is created and movie client search implemented by using useState for internal state management 4. Theme colours incorporated from figma design for form elements and html body

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import HelloWorld from "./Components/HelloWorld";
+import Counter from "./Components/Counter";
+import SearchBox from "./Components/SearchBox";
 
 function App() {
+  let movieList = [{
+    "id":1,
+    "name": "Spiderman: Home coming",
+    "genre": ["Super Hero", "Action"],
+    "year": "2017"
+  },{
+    "id":2,
+    "name": "The Intern",
+    "genre": ["Drama", "Feel Good"],
+    "year": "2015"
+  },{
+    "id":3,
+    "name": "Forrest Gump",
+    "genre": ["Drama", "Feel Good", "Action"],
+    "year": "1994"
+  },{
+    "id":4,
+    "name": "Captain America - Civil War",
+    "genre": ["Super Hero", "Action"],
+    "year": "2016"
+  },{
+    "id":5,
+    "name": "Taken 1",
+    "genre": ["CIA", "Action"],
+    "year": "2008"
+  },{
+    "id":6,
+    "name": "Taken 2",
+    "genre": ["CIA", "Action"],
+    "year": "2012"
+  },{
+    "id":7,
+    "name": "Taken 3",
+    "genre": ["CIA", "Action"],
+    "year": "2014"
+  },{
+    "id":8,
+    "name": "Safe House",
+    "genre": ["CIA", "Action"],
+    "year": "2012"
+  },{
+    "id":9,
+    "name": "Spider Man: Far from Home",
+    "genre": ["Super Hero", "Action"],
+    "year": "2019"
+  }]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelloWorld/>
+      <hr/><br/><br/>
+      <Counter/>
+      <hr/><br/><br/>
+      <SearchBox placeholder="Spiderman: Home Coming" initialData = { movieList } />
     </div>
   );
 }
