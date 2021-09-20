fb44202ffc6f2b909abea635ee9ab4c23c59947e
kesavakrishnanm <contact@krishnainfotron.com>
2021-09-20 19:33:19 +0530

    iHome Task 2 1. Removed npx create-react-app boiler plate code and built from scratch 2. Setup webpack dev and prod build setups 3. Updated readme accordingly

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import HelloWorld from "./Components/HelloWorld";
 import Counter from "./Components/Counter";
 import SearchBox from "./Components/SearchBox";
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "/build"),
+        filename: "index_bundle.js"
+    },
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader','css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
+}
